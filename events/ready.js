8a68178e7e523686d566c834bcee0dcd58523899
ClaireWhere <clairewhere4@gmail.com>
2024-01-03 23:07:51 -0500

    feat: clock in and out bot upon load

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,7 @@
 const { Client } = require("discord.js");
 const { logger } = require("../utils/logger");
+const { clockIn, clockOut } = require("../utils/timeclock.utils");
+const { dateToString } = require("../utils/utils");
 
 module.exports = {
     name: 'ready',
@@ -10,5 +12,9 @@ module.exports = {
      */
     execute(client) {
         logger.info(`Ready! Logged in as ${client.user.tag}`);
+        const time = clockOut(client.user.id);
+        logger.info(`Clocked out ${client.user.tag} after ${dateToString(time.sessionTime)}. Total time on the clock: ${dateToString(time.totalTime)}`);
+        clockIn(client.user.id);
+        logger.info(`Clocked in ${client.user.tag}`);
     },
 };
